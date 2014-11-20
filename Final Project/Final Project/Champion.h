322f5f5c1654e1f3556666796e751fe7f05f7031
jflinn18 <joeflinn412@gmail.com>
2014-11-20 14:11:19 -0800

    Updated UML
    Updated the UML for UI class and changed the class relationships.
    
    Also started to code the header file for the Champion class.

---
+++ Final Project/Final Project/Champion.h
@@ -0,0 +1,22 @@
+#ifndef _Champion_h
+#define _Champion_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Champion
+{
+public:
+	string _name;
+	string _primaryRole;
+	string _secondaryRole;
+	vector<string> _badChamps();
+	vector<string> _goodChamps();
+private:
+	
+};
+
+#endif
